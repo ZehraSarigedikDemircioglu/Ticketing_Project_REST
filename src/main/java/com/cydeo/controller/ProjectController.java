bd0432e1fc9216aef13646143f7c7c71c4bc43bf
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-09 11:48:22 -0400

    added @Tag and @Operation for swagger doc

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -3,6 +3,8 @@ package com.cydeo.controller;
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/api/v1/project")
+@Tag(name = "ProjectController",description = "Project API")
 public class ProjectController {
 
     private final ProjectService projectService;
@@ -21,18 +24,21 @@ public class ProjectController {
 
     @GetMapping
     @RolesAllowed("Manager")
+    @Operation(summary = "Get projects")
     public ResponseEntity<ResponseWrapper> getProjects() {
         return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved", projectService.listAllProjects(), HttpStatus.OK));
     }
 
     @GetMapping("/{projectCode}")
     @RolesAllowed("Manager")
+    @Operation(summary = "Get project by code")
     public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("projectCode") String code) {
         return ResponseEntity.ok(new ResponseWrapper("Project is retrieved", projectService.getByProjectCode(code), HttpStatus.OK));
     }
 
     @PostMapping
     @RolesAllowed({"Admin","Manager"})
+    @Operation(summary = "Create project")
     public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO projectDTO) {
         projectService.save(projectDTO);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully created", HttpStatus.CREATED));
@@ -40,6 +46,7 @@ public class ProjectController {
 
     @PutMapping
     @RolesAllowed("Manager")
+    @Operation(summary = "Update project")
     public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO projectDTO) {
         projectService.update(projectDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Project is successfully created",HttpStatus.CREATED));
@@ -47,6 +54,7 @@ public class ProjectController {
 
     @DeleteMapping("/{code}")
     @RolesAllowed("Manager")
+    @Operation(summary = "Delete project")
     public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("code") String code) {
         projectService.delete(code);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully deleted", HttpStatus.OK));
@@ -54,12 +62,14 @@ public class ProjectController {
 
     @GetMapping("/manager/project-status")
     @RolesAllowed("Manager")
+    @Operation(summary = "Get project by manager")
     public ResponseEntity<ResponseWrapper> getProjectByManager() {
         return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved", projectService.listAllProjectDetails(), HttpStatus.OK));
     }
 
     @PutMapping("/manager/complete/{projectCode}")
     @RolesAllowed("Manager")
+    @Operation(summary = "Complete project")
     public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String code) {
         projectService.complete(code);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully completed", HttpStatus.OK));
