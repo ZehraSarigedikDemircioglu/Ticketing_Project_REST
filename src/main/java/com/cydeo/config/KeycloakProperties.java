656bcea7fa7271c3e3bf86fc658a6bfeae9e58b0
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-09 10:38:12 -0400

    added keycloak properties

---
+++ src/main/java/com/cydeo/config/KeycloakProperties.java
@@ -0,0 +1,31 @@
+package com.cydeo.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class KeycloakProperties {
+    // why they create a separate class? Because, if I use realm value anywhere in different places by using this class. I do not need to re-declare these variables.
+
+    @Value("${keycloak.realm}")
+    private String realm;
+    @Value("${keycloak.auth-server-url}")
+    private String authServerUrl;
+    @Value("${keycloak.resource}")
+    private String clientId;
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+    @Value("${master.user}")
+    private String masterUser;
+    @Value("${master.user.password}")
+    private String masterUserPswd;
+    @Value("${master.realm}")
+    private String masterRealm;
+    @Value("${master.client}")
+    private String masterClient;
+
+}
